8dcb60ca1d2677d2f45809a8a6ab5d8aa43c7ad9
LEONARDO SOUZA DE CASTRO <uniflcastro@feinet.fei.edu.br>
2024-04-19 22:01:13 -0300

    bugs corrigidos

--- contatos.c
+++ contatos.c
@@ -6,7 +6,6 @@ Erro Criar(Contatos contatos[], int *pos){
     if(*pos >= TOTAL){
         return Maximo;
     }
-    Clear_buffer();
 
     do
     {
@@ -26,10 +25,12 @@ Erro Criar(Contatos contatos[], int *pos){
         fgets(contatos[*pos].Email, 70, stdin);
     } while (strlen(contatos[*pos].Email) == 1);
     
-
-    printf("Entre com seu telefone (não coloque caracteres especiais como () ou -): ");
-    fgets(contatos[*pos].Telefone, 12, stdin);
-
+    do
+    {
+        printf("Entre com seu telefone (não coloque caracteres especiais como () ou -): ");
+        fgets(contatos[*pos].Telefone, 12, stdin);
+    } while ((strlen(contatos[*pos].Telefone) <= 1));
+    
     contatos[*pos].Nome[strcspn(contatos[*pos].Nome, "\n")] = 0;
     contatos[*pos].Sobrenome[strcspn(contatos[*pos].Sobrenome, "\n")] = 0;
     contatos[*pos].Email[strcspn(contatos[*pos].Email, "\n")] = 0;
@@ -49,14 +50,14 @@ Erro Deletar(Contatos contatos[], int *pos){
         return Sem_contatos;
     }
 
-    char num_deletar[11];
+    char num_deletar[13];
 
 
     printf("Entre com o numero do contato a ser deletado (nao utilize () ou - ao digitar) sera deletado o primeiro resultado com este numero: ");
-    fgets(contatos[*pos].Telefone, 11, stdin);
+    fgets(num_deletar, 12, stdin);
 
-    contatos[*pos].Telefone[strcspn(contatos[*pos].Telefone, "\n")] = 0;
-    Clear_buffer();
+    num_deletar[strcspn(num_deletar, "\n")] = 0;
+
 
     int pos_deletar = 0;
     int i = 0;
@@ -65,13 +66,28 @@ Erro Deletar(Contatos contatos[], int *pos){
     {
         if (strcmp(num_deletar, contatos[i].Telefone) == 0)
         {
-            pos_deletar = i-1;
+            printf("Posicao: %d", i+1);
+            printf("\t Nome do Contato: %s %s", contatos[i].Nome, contatos[i].Sobrenome);
+            printf("\t Email: %s", contatos[i].Email);
+            printf("\t Telefone: %s \n", contatos[i].Telefone);
         }
     }
+
+    printf("Qual posicao do contato a ser deletado? \n");
+    scanf("%d", &pos_deletar);
+
+    // for (i; i < *pos; i++)
+    // {
+    //     if (strcmp(num_deletar, contatos[i].Telefone) == 0)
+    //     {
+    //         pos_deletar = i-1;
+    //     }
+    // }
     
 
-    for (i = pos_deletar; i < *pos; i++)
+    for (i = pos_deletar-1; i < *pos; i++)
     {
+        printf("%d", i);
         strcpy(contatos[i].Nome, contatos[i+1].Nome);
         strcpy(contatos[i].Sobrenome, contatos[i+1].Sobrenome);
         strcpy(contatos[i].Email, contatos[i+1].Email);
@@ -97,7 +113,7 @@ Erro Listar(Contatos contatos[], int pos){
         printf("\t Email: %s", contatos[i].Email);
         printf("\t Telefone: %s \n", contatos[i].Telefone);
     }
-    
+     return Ok;
     
 }
 
