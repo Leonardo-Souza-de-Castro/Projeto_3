180bdb90e94c647c76f91f97226fac21ceef2192
Leonardo <leonardo030605@gmail.com>
2024-05-19 17:52:21 -0300

    validação de telefone único funcionando

--- contatos.c
+++ contatos.c
@@ -2,8 +2,12 @@
 #include <string.h>
 #include "contatos.h"
 
-Erro Criar(Contatos contatos[], int *pos){
-    if(*pos >= TOTAL){
+Erro Criar(Contatos contatos[], int *pos)
+{
+    char num_add[13];
+
+    if (*pos >= TOTAL)
+    {
         return Maximo;
     }
 
@@ -24,27 +28,42 @@ Erro Criar(Contatos contatos[], int *pos){
         printf("Entre com seu email: ");
         fgets(contatos[*pos].Email, 70, stdin);
     } while (strlen(contatos[*pos].Email) == 1);
-    
+
+    int i = 0;
+    int pos_cont_exis = 0;
     do
     {
-        printf("Entre com seu telefone (não coloque caracteres especiais como () ou -): ");
-        fgets(contatos[*pos].Telefone, 12, stdin);
-    } while ((strlen(contatos[*pos].Telefone) <= 1));
-    
+        printf("Entre com seu telefone (nao coloque caracteres especiais como () ou -): ");
+        scanf("%[^\n]", num_add);
+        Clear_buffer();
+
+        i = 0;
+        pos_cont_exis = 0;
+        for (i; i < *pos; i++)
+        {
+            if (strcmp(num_add, contatos[i].Telefone) == 0)
+            {
+                printf("Erro: Este numero ja existe na lista de contatos\n");
+                pos_cont_exis = i;
+            }
+        }
+        
+
+    } while (strcmp(num_add, contatos[pos_cont_exis].Telefone) == 0);
+
     contatos[*pos].Nome[strcspn(contatos[*pos].Nome, "\n")] = 0;
     contatos[*pos].Sobrenome[strcspn(contatos[*pos].Sobrenome, "\n")] = 0;
     contatos[*pos].Email[strcspn(contatos[*pos].Email, "\n")] = 0;
-    contatos[*pos].Telefone[strcspn(contatos[*pos].Telefone, "\n")] = 0;
-
-    Clear_buffer();
+    num_add[strcspn(num_add, "\n")] = 0;
+    strcpy(contatos[*pos].Telefone, num_add);
 
     *pos = *pos + 1;
 
     return Ok;
-
 }
 
-Erro Deletar(Contatos contatos[], int *pos){
+Erro Deletar(Contatos contatos[], int *pos)
+{
     if (*pos == 0)
     {
         return Sem_contatos;
@@ -52,12 +71,10 @@ Erro Deletar(Contatos contatos[], int *pos){
 
     char num_deletar[13];
 
-
     printf("Entre com o numero do contato a ser deletado (nao utilize () ou - ao digitar) sera deletado o primeiro resultado com este numero: ");
     fgets(num_deletar, 12, stdin);
 
     num_deletar[strcspn(num_deletar, "\n")] = 0;
-
 
     int pos_deletar = 0;
     int i = 0;
@@ -66,7 +83,7 @@ Erro Deletar(Contatos contatos[], int *pos){
     {
         if (strcmp(num_deletar, contatos[i].Telefone) == 0)
         {
-            printf("Posicao: %d", i+1);
+            printf("Posicao: %d", i + 1);
             printf("\t Nome do Contato: %s %s", contatos[i].Nome, contatos[i].Sobrenome);
             printf("\t Email: %s", contatos[i].Email);
             printf("\t Telefone: %s \n", contatos[i].Telefone);
@@ -76,21 +93,22 @@ Erro Deletar(Contatos contatos[], int *pos){
     printf("Qual posicao do contato a ser deletado? \n");
     scanf("%d", &pos_deletar);
 
-    for (i = pos_deletar-1; i < *pos; i++)
+    for (i = pos_deletar - 1; i < *pos; i++)
     {
         printf("%d", i);
-        strcpy(contatos[i].Nome, contatos[i+1].Nome);
-        strcpy(contatos[i].Sobrenome, contatos[i+1].Sobrenome);
-        strcpy(contatos[i].Email, contatos[i+1].Email);
-        strcpy(contatos[i].Telefone, contatos[i+1].Telefone);
+        strcpy(contatos[i].Nome, contatos[i + 1].Nome);
+        strcpy(contatos[i].Sobrenome, contatos[i + 1].Sobrenome);
+        strcpy(contatos[i].Email, contatos[i + 1].Email);
+        strcpy(contatos[i].Telefone, contatos[i + 1].Telefone);
     }
 
-    *pos = *pos-1;
+    *pos = *pos - 1;
 
     return Ok;
 }
 
-Erro Listar(Contatos contatos[], int pos){
+Erro Listar(Contatos contatos[], int pos)
+{
     if (pos == 0)
     {
         return Sem_contatos;
@@ -99,19 +117,20 @@ Erro Listar(Contatos contatos[], int pos){
     int i = 0;
     for (i; i < pos; i++)
     {
-        printf("Posicao: %d", i+1);
+        printf("Posicao: %d", i + 1);
         printf("\t Nome do Contato: %s %s", contatos[i].Nome, contatos[i].Sobrenome);
         printf("\t Email: %s", contatos[i].Email);
         printf("\t Telefone: %s \n", contatos[i].Telefone);
     }
-     return Ok;
-    
+    return Ok;
 }
 
-Erro Salvar(Contatos contatos[],int total, int pos){
+Erro Salvar(Contatos contatos[], int total, int pos)
+{
     FILE *f = fopen("contatos", "wb");
 
-    if(f == NULL){
+    if (f == NULL)
+    {
         return Abrir;
     }
 
@@ -124,7 +143,8 @@ Erro Salvar(Contatos contatos[],int total, int pos){
 
     erro = fwrite(&pos, 1, sizeof(int), f);
 
-    if(erro <= 0){
+    if (erro <= 0)
+    {
         return Escrever;
     }
 
@@ -137,32 +157,40 @@ Erro Salvar(Contatos contatos[],int total, int pos){
     return Ok;
 }
 
-Erro Carregar(Contatos contatos[],int total, int *pos){
+Erro Carregar(Contatos contatos[], int total, int *pos)
+{
     FILE *f = fopen("contatos", "rb");
 
-    if(f == NULL){
+    if (f == NULL)
+    {
         return Abrir;
     }
 
     int erro = fread(contatos, total, sizeof(Contatos), f);
-    if(erro <= 0){
+    if (erro <= 0)
+    {
         return Ler;
     }
 
     erro = fread(pos, 1, sizeof(int), f);
-    if(erro <= 0){
+    if (erro <= 0)
+    {
         return Ler;
     }
 
     erro = fclose(f);
-    if(erro != 0){
+    if (erro != 0)
+    {
         return Fechar;
     }
 
     return Ok;
 }
 
-void Clear_buffer(){
+void Clear_buffer()
+{
     int c;
-    while ((c = getchar()) != '\n' && c != EOF) { } //Faz a leitura de tudo que for digitado até que c seja diferente de \n ou EOF(Final do arquivo).
+    while ((c = getchar()) != '\n' && c != EOF)
+    {
+    } // Faz a leitura de tudo que for digitado até que c seja diferente de \n ou EOF(Final do arquivo).
 }
